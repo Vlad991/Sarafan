ca18e814a401df5c4916dfdb4c57ed18ca78382a
vlad <vladkuzma99@gmail.com>
2019-07-31 20:26:55 +0300

    Add spring data

--- src/main/java/com/letscode/sarafan/domain/Message.java
+++ src/main/java/com/letscode/sarafan/domain/Message.java
@@ -1,8 +1,11 @@
 package com.letscode.sarafan.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonView;
 import lombok.*;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Table
@@ -13,6 +16,14 @@ import javax.persistence.*;
 public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonView(Views.Id.class)
     private Long id;
+
+    @JsonView(Views.IdName.class)
     private String text;
+
+    @Column(updatable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonView(Views.FullMessage.class)
+    private LocalDateTime creationDate;
 }
