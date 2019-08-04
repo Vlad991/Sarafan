6e3563ecc42144539d7a144a63d16d095a44f920
vlad <vladkuzma99@gmail.com>
2019-08-04 17:15:12 +0300

    Add application.properties

--- src/main/java/com/letscode/sarafan/config/WebSecurityConfig.java
+++ src/main/java/com/letscode/sarafan/config/WebSecurityConfig.java
@@ -2,13 +2,17 @@ package com.letscode.sarafan.config;
 
 import com.letscode.sarafan.domain.User;
 import com.letscode.sarafan.repository.UserDetailsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
 
 import java.time.LocalDateTime;
 
@@ -16,6 +20,7 @@ import java.time.LocalDateTime;
 @EnableWebSecurity
 @EnableOAuth2Sso
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,5 +1,6 @@
+
 function getIndex(list, id) {
-    for (var i = 0; i < list.length; i++) {
+    for (var i = 0; i < list.length; i++ ) {
         if (list[i].id === id) {
             return i;
         }
@@ -13,14 +14,14 @@ var messageApi = Vue.resource('/message{/id}');
 
 Vue.component('message-form', {
     props: ['messages', 'messageAttr'],
-    data: function () {
+    data: function() {
         return {
             text: '',
             id: ''
         }
     },
     watch: {
-        messageAttr: function (newVal, oldVal) {
+        messageAttr: function(newVal, oldVal) {
             this.text = newVal.text;
             this.id = newVal.id;
         }
@@ -31,8 +32,8 @@ Vue.component('message-form', {
         '<input type="button" value="Save" @click="save" />' +
         '</div>',
     methods: {
-        save: function () {
-            var message = {text: this.text};
+        save: function() {
+            var message = { text: this.text };
 
             if (this.id) {
                 messageApi.update({id: this.id}, message).then(result =>
@@ -65,14 +66,12 @@ Vue.component('message-row', {
         '</span>' +
         '</div>',
     methods: {
-        edit: function () {
+        edit: function() {
             this.editMethod(this.message);
         },
-        del: function () {
+        del: function() {
             messageApi.remove({id: this.message.id}).then(result => {
-                if(result.ok
-        )
-            {
+                if (result.ok) {
                 this.messages.splice(this.messages.indexOf(this.message), 1)
             }
         })
@@ -82,7 +81,7 @@ Vue.component('message-row', {
 
 Vue.component('messages-list', {
     props: ['messages'],
-    data: function () {
+    data: function() {
         return {
             message: null
         }
@@ -93,16 +92,15 @@ Vue.component('messages-list', {
         '<message-row v-for="message in messages" :key="message.id" :message="message" ' +
         ':editMethod="editMethod" :messages="messages" />' +
         '</div>',
-    created: function () {
+    created: function() {
         messageApi.get().then(result =>
         result.json().then(data =>
-        data.forEach(message => this.messages.push(message)
-    )
+        data.forEach(message => this.messages.push(message))
     )
     )
     },
     methods: {
-        editMethod: function (message) {
+        editMethod: function(message) {
             this.message = message;
         }
     }
